3c3302493c0aa27b5e7e3141ef5cc20cffb281a6
Iyanuoluwa Oluwatade <iyanuoluwaoluwatade@gmail.com>
2025-08-14 01:27:25 +0100

    feat: home page design

--- src/components/getInTouch.jsx
+++ src/components/getInTouch.jsx
@@ -4,9 +4,7 @@ const GetInTouch = () => {
             <h1 className="text-[#FFFFFF] text-xl m-[12px] text-Raleway">Get In Touch</h1>
         <p className="text-[#9C9C9C] t mb-2rem text-sm text-Helvetica w-[80%    ]">If you have any questions or would like to collaborate, feel free to reach out! Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro atque harum, incidunt ex reprehenderit recusandae doloremque veniam sit odit ut aliquid cupiditate deserunt sapiente similique odio! Enim, quam nostrum. Fugiat.</p>
         <div>
-            <Form>
-                <input type="text" placeholder="Your Name" className="w-[80%] p-2 mb-4 border border-gray-300 rounded-md" />
-            </Form>
+           
         </div>
         </div>
         
